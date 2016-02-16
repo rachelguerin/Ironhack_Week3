da0be0c0584a4bf084145a8a31db968c081d4da4
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-16 22:36:37 +0100

    Ceasar cypher

---
+++ js/ceasar.js
@@ -0,0 +1,34 @@
+function ceasarCipher (message,shift) {
+	shift = shift || 3
+	return message.split('').map(function (char){
+		return isUpper(char) || isLower(char)
+			? getChar(char,shift)
+			: char 
+	}).reduce(function (cipher,char) {
+		return cipher + char;
+	},"")
+}
+
+function getChar(char,shift) {
+	charCode = char.charCodeAt(0);
+	charCodeShifted = charCode + shift;
+
+	if ((isUpper(char) && charCodeShifted > 90) || (isLower(char) && charCodeShifted > 122)) {
+		charCodeShifted -= 26;
+	} else if ((isUpper(char) && charCodeShifted < 65) || (isLower(char) && charCodeShifted < 97)) {
+		charCodeShifted += 26;
+	} 
+	return String.fromCharCode(charCodeShifted);
+
+}
+
+function isUpper(char) {
+	return char >= 'A' && char <= 'Z'; 
+}
+
+function isLower(char) {
+	return char >= 'a' && char <= 'z';
+}
+
+
+console.log(ceasarCipher("Et tu, brute?",6));
