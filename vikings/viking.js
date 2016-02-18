d160b45cc854b723c2abc2b54d81f4a43111b199
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-18 15:29:55 +0100

    Vikings fight eachother

---
+++ vikings/viking.js
@@ -0,0 +1,73 @@
+var Viking = function(name,health,strength){
+	this.name = name;
+	this.health = health;
+	this.strength = strength;
+	//console.log("Viking "+this.name+" has entered the pit.");
+}
+
+var Pit = function(name,turns){
+	this.name = name
+	this.players = [];
+	this.turns = turns;
+	
+}
+
+Pit.prototype.attack = function(){
+	//choose p1 or p2
+	if (Math.random() < 0.5){
+		attacker = this.players[0];
+		defender = this.players[1];
+	} else {
+		attacker = this.players[1];
+		defender = this.players[0];
+	}
+
+	damage = attacker.strength * 0.2;
+	defender.health -= damage;
+	console.log(attacker.name+" has attacked "+defender.name+".");
+	console.log(defender.name+" has suffered "+damage+" damage. His health is now "+defender.health+".");
+	if (defender.health > 5){
+		return true;
+	}
+	else{
+		console.log(defender.name + " has surrendered!!.");
+		return false;
+	}
+}
+
+Pit.prototype.add_player = function(player){
+	this.players.push(player);
+}
+Pit.prototype.print_status = function(){
+	console.log(this.players[0].name + " and "+this.players[1].name+" have entered the pit at "+this.name+". \nThe crowd roars with anticipation.");
+};
+
+Pit.prototype.fight = function(){
+	for(i=0;i<this.turns;i++){
+		if (this.attack()) {
+			continue;
+		} else { 
+			console.log ("");
+			break; }
+	}
+	var winner=this.players[0].health > this.players[1].health ? this.players[0] : this.players[1];
+	console.log("The fight is OVER. The winner is " + winner.name);
+}
+
+var vlad = new Viking('Vlad',20,20);
+
+var ragnar = new Viking('Ragnar',20,20);
+
+var elgin = new Pit("Elgin",10);
+
+elgin.add_player(vlad);
+elgin.add_player(ragnar);
+elgin.print_status();
+elgin.fight();
+
+
+
+
+
+
+
